2b2cc3890a06d06f87b29571f453db2e9074657b
sarahdenkert01-hue <sarahdenkert01@gmail.com>
2026-01-26 13:43:35 -0600

    Replace Spinner component with Loader component

--- src/pages/SessionsPage.tsx
+++ src/pages/SessionsPage.tsx
@@ -120,7 +120,7 @@ export default function SessionsPage() {
   if (loading) {
     return (
       <div className="flex items-center justify-center h-[60vh]">
-        <Spinner size="lg" />
+        <Loader size="lg" />
       </div>
     )
   }
