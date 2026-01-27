7a6269c460d0836c71b76aa609c92f920443279c
therapy-creations <sarahdenkert@sbcglobal.net>
2026-01-27 12:53:57 -0600

    Fix import case for Loader component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { toast } from 'sonner'
-import { Loader } from '@/components/ui/loader'
+import { Loader } from '@/components/ui/Loader'
 import { format } from 'date-fns'
 
 export default function HomePage() {
