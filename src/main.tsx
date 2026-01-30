d0be7f1c3720d7fac99ce03524cbf2e43d774a0d
therapy-creations <sarahdenkert@sbcglobal.net>
2026-01-30 14:05:39 -0600

    Reorganize imports in main.tsx
    Reorder imports to keep CSS import at the bottom.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'next-themes'
-import { Toaster } from './components/ui/sonner'
 import App from './App'
-import './index.css'
+import { Toaster } from './components/ui/sonner'
+import './index.css' // Keep this at the absolute bottom of imports
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
